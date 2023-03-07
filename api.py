380061c6f7970da4221f59d4d5712ce0ab0abd93
Paulo Diniz <paulo.diniz@ipdec.org>
2023-03-07 15:33:37 -0400

    Title, price and availability extraction

--- api.py
+++ api.py
@@ -1,41 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from pymongo import MongoClient
+import datetime
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.Estudo_MongoDB
+
+     
 
 lista_produtos = []
-
+cont = 1
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-url_base = "https://www.kabum.com.br"
-start_url = "https://www.kabum.com.br/tv"
-
+#start_url = "https://www.kabum.com.br/busca/gabinete-gamer-Hayom?page_number=" + str(cont) +"&page_size=20&facet_filters=&sort=most_searched"
+start_url = "https://www.kabum.com.br/busca/monitor-aoc?page_number=" + str(cont) + "&page_size=20&facet_filters=&sort=most_searched"
+print(start_url)
 page = requests.get(start_url,headers=headers)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-tv_pages = []
-for link in soup.find_all('a'):
-    href = link.get('href')
-    if href and '/tv' in href:
-        tv_pages.append(href)
-        
-for tv_page in tv_pages:
-   
-    page_tv = requests.get(tv_page, headers=headers)
+while(len(soup.find_all('div', class_="sc-9e19be64-0 jPozmn")) == 0):
 
-    soup_tv = BeautifulSoup(page_tv.content, 'html.parser')
-
-
-    produtos = soup_tv.find_all('div', class_='sc-1fcmfeb-2 jXZzWW')
-    print(produtos)
+    start_url = "https://www.kabum.com.br/busca/monitor-aoc?page_number=" + str(cont) + "&page_size=20&facet_filters=&sort=most_searched"
+    cont = cont + 1;
+    page = requests.get(start_url,headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    produtos = soup.find_all('div', class_='sc-ff8a9791-7 hDHAaY productCard')
 
     for produto in produtos:
         titulo = produto.find('span', class_='sc-d99ca57-0 cpPIRA sc-ff8a9791-16 dubjqF nameCard').text
-        preco = produto.find('span', class_='sc-3b515ca1-2 eqqhbT priceCard').text
-        preco = preco.replace('\xa0', '').replace(',', '.')
-        # print(preco)
-        
-        produto_dict = {'titulo': titulo, 'preco': preco}
-        lista_produtos.append(produto_dict)
+        print(titulo)
+        product_category = titulo.split(' ')[0]
 
+        if(product_category == "Monitor"):
+            if(produto.find('span', class_='sc-3b515ca1-2 eqqhbT priceCard') == None):
+                preco = "R$00,0"
+                disponibilidade = "Sem estoque"
+            else:
+                preco = produto.find('span', class_='sc-3b515ca1-2 eqqhbT priceCard').text
+                preco = preco.replace('\xa0', '').replace(',', '.')
+                disponibilidade = "Disponível"
+            
+            
+            
+            
+            produto_dict = {
+                'titulo': titulo,
+                'preco': preco,
+                'disponibilidade': disponibilidade,
+                'create_datetime': datetime.datetime.now()
+                }
+            db.teste.insert_one(produto_dict)
+            
+            lista_produtos.append(produto_dict)
+        
+    
     print(len(lista_produtos))
 # print(lista)
